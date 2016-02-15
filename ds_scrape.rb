8ca5b35ec78658981c35129010264b43e5b9eae6
progdrum <brookforestscitech@gmail.com>
2016-02-15 00:22:39 -0700

    Committing what is here in prep for do-over

--- ds_scrape.rb
+++ ds_scrape.rb
@@ -1,10 +1,18 @@
 require 'rest-client'
 require 'nokogiri'
+require 'json'
 
 # THIS SEEMS TO WORK SO FAR, BUT ISN'T GETTING COMMENTS! Use the questions/{id}/comments
 # API method to get these!
 
+# UPDATE: I don't need to scrape at all! I should just be able to use the API straight up!
+# Between the API and JSON gem, I should be able to easily get all the comments and answers.
+# From there, I can use R to process the shit out of the gathered text!
+
+API_BASE = 'https://api.stackexchange.com/2.2/'
+
 data_science = Nokogiri::HTML(RestClient.get('http://stats.stackexchange.com/questions/195034'))
+comments = RestClient.get(API_BASE + 'questions/195034/comments?site=stats')
 
 # Extract the question and answer text and list text
 qa_text = data_science.css('.post-text p')
